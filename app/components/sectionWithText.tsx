e8b5ddb23c65f60563ef0357fafa2d2d842d4fc3
Martin Skatvedt <martinskatvedt@gmail.com>
2024-08-19 22:00:13 +0200

    feat: animation

---
+++ app/components/sectionWithText.tsx
@@ -0,0 +1,60 @@
+import SectionContainer from "./sectionContainer";
+import Image from "next/image";
+import { motion, useInView } from "framer-motion";
+import { ReactNode, useRef } from "react";
+import { cn } from "@/utils/utils";
+
+type SectionWithTextProps = {
+  className?: string;
+
+  title: string;
+  text: ReactNode | string;
+
+  isImLeft?: boolean;
+  imSrc: string;
+  imAlt: string;
+  imHeight: number;
+  imWidth: number;
+};
+
+const SectionWithText = (props: SectionWithTextProps) => {
+  const { className, title, text, imSrc, imAlt, imHeight, imWidth, isImLeft } =
+    props;
+  const ref = useRef<HTMLDivElement>(null);
+  const isInView = useInView(ref, { once: true });
+
+  return (
+    <SectionContainer
+      className={cn(
+        "flex flex-col sm:flex-row items-center justify-between h-auto gap-8 sm:gap-0 py-4 sm:py-0",
+        className
+      )}
+    >
+      <div
+        className={cn(
+          "text-center flex flex-col gap-10 items-center justify-center w-full transition-all duration-[1s]",
+          isInView ? "opacity-100" : "opacity-0"
+        )}
+        ref={ref}
+      >
+        <h2 className="font-normal text-6xl">{title}</h2>
+        <p className="w-full sm:w-[70%] xl:w-[50%] mx-auto font-normal text-xl px-4">
+          {text}
+        </p>
+      </div>
+
+      <div className="w-3/5">
+        <Image
+          src={imSrc}
+          alt={imAlt}
+          className="object-cover object-center w-full max-h-[90vh]"
+          width={imWidth}
+          height={imHeight}
+          quality={100}
+        />
+      </div>
+    </SectionContainer>
+  );
+};
+
+export default SectionWithText;
--- app/page.tsx
+++ app/page.tsx
@@ -4,17 +4,19 @@ import Image from "next/image";
 import BigLogo from "./components/logo";
 import SectionContainer from "./components/sectionContainer";
 import { motion } from "framer-motion";
+import SectionWithText from "./components/sectionWithText";
 
 const Home = () => {
   return (
     <main className="flex flex-col w-full">
-      <SectionContainer className="relative w-full h-[100vh]">
+      <SectionContainer className="relative w-full mb-[5vh] min-h-[95vh]">
         <Image
-          src="/images/sea.jfif"
+          src="/images/hero.png"
           alt="Picture of view of the sea"
           priority
           fill
           className="object-cover max-h-[95vh] object-center w-full"
+          quality={100}
         />
         <motion.div
           layout
@@ -31,30 +33,20 @@ const Home = () => {
         </motion.div>
       </SectionContainer>
 
-      <SectionContainer className="flex flex-col sm:flex-row items-center justify-between h-auto gap-8 sm:gap-0 py-4 sm:py-0">
-        <div className="text-center flex flex-col gap-10 items-center justify-center w-full">
-          <h2 className="font-normal text-6xl">Velkommen!</h2>
-          <p className="w-full sm:w-[50%] xl:w-[30%] mx-auto font-normal text-xl px-4">
-            Rorbuen ligger på Moster i Bømlo kommune, og har en størrelse på 110
+      <SectionWithText
+        title="Velkommen!"
+        text="Rorbuen ligger på Moster i Bømlo kommune, og har en størrelse på 110
             kvm fordelt på to etasjer. I tillegg har den en hems på 13 kvm.
             Flott uteområde på bakkeplan, samt en terrasse på ca 12 kvm i 2.
-            etasje.
-          </p>
-        </div>
-        <div className="w-3/5">
-          <Image
-            src="/images/cabin_from_sea.png"
-            alt="Picture of view of the pier"
-            className="object-cover object-center w-full max-h-[90vh]"
-            width={437}
-            height={597}
-            quality={100}
-          />
-        </div>
-      </SectionContainer>
+            etasje."
+        imSrc="/images/velkommen.png"
+        imAlt="Picture of the cabin from the sea"
+        imHeight={437}
+        imWidth={597}
+      />
 
       <SectionContainer className="bg-[#D3E1E5] flex gap-8 py-4 sm:py-24 px-2 sm:px-20 flex-col sm:flex-row">
-        <div className="relative w-full sm:w-1/3 bg-red-500 h-full">
+        <div className="relative w-full sm:w-1/3 h-full">
           <Image
             src="/images/living_room.png"
             alt="Picture of the living room"
@@ -84,47 +76,30 @@ const Home = () => {
         </div>
       </SectionContainer>
 
-      <SectionContainer className="flex flex-col sm:flex-row items-center justify-between h-auto gap-8 sm:gap-0 flex-wrap-reverse sm:flex-nowrap py-4 sm:py-0">
-        <div className="w-3/5 bg-green-500">
-          <Image
-            src="/images/cabin.png"
-            alt="Picture of view of the pier"
-            className="object-cover object-center w-full max-h-[90vh]"
-            width={708}
-            height={814}
-            quality={100}
-          />
-        </div>
-        <div className="text-center flex flex-col gap-10 items-center justify-center w-full">
-          <h2 className="font-normal text-6xl">Innhold</h2>
-          <p className="w-full sm:w-[50%] xl:w-[30%] mx-auto font-normal text-xl px-4">
-            Første etasje inneholder entré, 2 soverom, 1 bad og stue + kjøkken.
-            Andre etasje inneholder 1 soverom, 1 bad + stue og kjøkken. Rorbuen
-            er fullt møblert, godt utstyrt og har en høy standard. Den har
-            elbil-lader og 3 parkeringsplasser.
-          </p>
-        </div>
-      </SectionContainer>
+      <SectionWithText
+        className="sm:flex-row-reverse"
+        title="Innhold"
+        text="Første etasje inneholder entré, 2 soverom, 1 bad og stue + kjøkken. Andre etasje inneholder 1 soverom, 1 bad + stue og kjøkken. Rorbuen er fullt møblert, godt utstyrt og har en høy standard. Den har elbil-lader og 3 parkeringsplasser."
+        imSrc="/images/innhold.png"
+        imAlt="Picture of the cabin"
+        imHeight={814}
+        imWidth={708}
+      />
 
-      <SectionContainer className="bg-[#E1E5D3] flex flex-col sm:flex-row items-center justify-between h-auto gap-8 sm:gap-0 py-4 sm:py-0">
-        <div className="text-center flex flex-col gap-10 items-center justify-center w-full">
-          <h2 className="font-normal text-6xl">Booking</h2>
-          <p className="w-full sm:w-[50%] xl:w-[30%] mx-auto font-normal text-xl px-4">
+      <SectionWithText
+        className="bg-[#E1E5D3]"
+        title="Booking"
+        text={
+          <>
             Ved booking eller andre spørsmål, kontakt Åse Jensen på tlf{" "}
             <a href="tel:+4790038174">900 36 174</a>.
-          </p>
-        </div>
-        <div className="w-3/5">
-          <Image
-            src="/images/sunny-sea.jfif"
-            alt="Picture of view of the pier"
-            className="object-cover object-center w-full max-h-[90vh]"
-            width={599}
-            height={668}
-            quality={100}
-          />
-        </div>
-      </SectionContainer>
+          </>
+        }
+        imSrc="/images/booking.png"
+        imAlt="Picture of view of the pier"
+        imWidth={599}
+        imHeight={668}
+      />
     </main>
   );
 };
